f7b3ef5344f00216e88dd1c914b00b24ee1e8884
BeckhamDev <guilhermebeckham14@hotmail.com>
2022-12-30 10:12:05 -0300

    Adding the product's name to the list of fornecedors

--- app_super_gestao/app/Models/Fornecedor.php
+++ app_super_gestao/app/Models/Fornecedor.php
@@ -10,4 +10,12 @@ class Fornecedor extends Model
 {
     protected $table = 'fornecedors';
     protected $fillable = ['nome', 'site', 'uf', 'email'];
+
+    /**
+     * Retorna os produtos que cada fornecedor tem
+     */
+    public function produtos()
+    {
+        return $this->hasMany(Produto::class, 'fornecedor_id', 'id');
+    }
 }
